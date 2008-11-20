bdc446d3083e1dc7e9a112e9ba0563ee7eca6ca5
sgrossnw <sgrossnw>
2008-11-20 11:21:28 +0000

    a first skeleton program/test, build script, some inteded third-party libraries

---
+++ jlk/src/de/evjnw/jlk/LiedBrowser.java
@@ -0,0 +1,36 @@
+/* 
+   JLK - Java Lieder Katalog
+   Copyright 2008, Stephan Gross
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   $Id: LiedBrowser.java,v 1.1 2008/11/20 11:21:28 sgrossnw Exp $
+ */
+package de.evjnw.jlk;
+
+/**
+ * This class is the entry point to the JLK application
+ * 
+ * @author Stephan
+ */
+public class LiedBrowser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jlk/tests/de/evjnw/jlk/LiedBrowserTest.java
@@ -0,0 +1,35 @@
+/* 
+   JLK - Java Lieder Katalog
+   Copyright 2008, Stephan Gross
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   $Id: LiedBrowserTest.java,v 1.1 2008/11/20 11:21:28 sgrossnw Exp $
+ */
+package de.evjnw.jlk;
+
+import junit.framework.TestCase;
+
+/**
+ * This minimalistic unit test is needed by the build script.
+ * It should test the correct setup of the application.  
+ * 
+ * @author Stephan
+ */
+public class LiedBrowserTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
