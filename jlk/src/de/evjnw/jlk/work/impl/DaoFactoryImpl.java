ac1d440a3c8d6b212c9ad8646321e08e8368bb07
sgrossnw <sgrossnw>
2009-03-21 21:00:16 +0000

    Flag für den Schema Export

--- jlk/src/de/evjnw/jlk/work/impl/DaoFactoryImpl.java
+++ jlk/src/de/evjnw/jlk/work/impl/DaoFactoryImpl.java
@@ -14,7 +14,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-   $Id: DaoFactoryImpl.java,v 1.3 2009/03/21 15:28:06 ma08 Exp $
+   $Id: DaoFactoryImpl.java,v 1.4 2009/03/21 21:00:16 sgrossnw Exp $
  */
 package de.evjnw.jlk.work.impl;
 
@@ -28,47 +28,56 @@ import de.evjnw.jlk.work.dao.BenutzerDao;
 import de.evjnw.jlk.work.dao.DaoFactory;
 
 /**
- * Diese Klasse bietet die Hibernate-basierte Implementierung
- * der {@link DaoFactory}.
- * Sie implementiert auch das <em>Factory Pattern</em> und hilft
- * so bei der Strukturierung der Applikation. 
+ * Diese Klasse bietet die Hibernate-basierte Implementierung der
+ * {@link DaoFactory}. Sie implementiert auch das <em>Factory Pattern</em> und
+ * hilft so bei der Strukturierung der Applikation.
  * 
  * @author Stephan
  */
 public class DaoFactoryImpl implements DaoFactory {
 
 	private SessionFactory factory;
-	
+
+	/** 
+	 * Mit diesem Flag könnnen wir steuern, ob während der Entwicklungsphase 
+	 * automatisch die Datenbank-Skripte erzeugt werden sollen.
+	 */
+	private boolean exportSchemaForTests = false;
+
 	/**
 	 * Benutzer DAO.
 	 */
-	private BenutzerDaoImpl benutzerDao; 
+	private BenutzerDaoImpl benutzerDao;
 
 	/**
-	 * @param user  technischer Benutzer an der Datenbank
-	 * @param password  Passwort des technischen Benutzers an der Datenbank
+	 * @param user
+	 *            technischer Benutzer an der Datenbank
+	 * @param password
+	 *            Passwort des technischen Benutzers an der Datenbank
 	 */
 	public DaoFactoryImpl(String user, String password) {
 		// TODO: sollte eigentlich eine Ressource im Classpath sein
 		File f = new File("hibernate.cfg.xml");
-		Configuration configuration =
-            new Configuration().configure(f);
+		Configuration configuration = new Configuration().configure(f);
 		// TODO: Benutzer und Passwort in der Configuration ersetzen
-		
-		//		configuration.setInterceptor(new LastModifiedInterceptor());
-		/*
-		SchemaExport export =
-            new SchemaExport(configuration);
-		export.create(false, true);
-		*/
+
+		// configuration.setInterceptor(new LastModifiedInterceptor());
+
+		if (exportSchemaForTests) {
+			SchemaExport export = new SchemaExport(configuration);
+			export.setOutputFile("create_jlk.sql");
+			boolean writeScript = true;
+			boolean executeDdl = true;
+			export.create(writeScript, executeDdl);
+		}
+
 		factory = configuration.buildSessionFactory();
-		
+
 		benutzerDao = new BenutzerDaoImpl();
 		benutzerDao.setFactory(factory);
-		
 		// TODO: die anderen DAOs initialisieren
 	}
-	
+
 	/**
 	 * @see de.evjnw.jlk.work.dao.DaoFactory#getBenutzerDao()
 	 */
