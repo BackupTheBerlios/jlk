e6f2412032e576be2c8ba54f90a819588a8a5df8
sgrossnw <sgrossnw>
2009-08-15 11:38:31 +0000

    22.7. der erste View

---
+++ jlk/src/de/evjnw/jlk/ui/impl/NewModelView.java
@@ -0,0 +1,54 @@
+/* 
+   JLK - Java Lieder Katalog
+   Copyright 2009, Stephan Gross
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   $Id: NewModelView.java,v 1.1 2009/08/15 11:38:31 sgrossnw Exp $
+ */
+package de.evjnw.jlk.ui.impl;
+
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import org.apache.log4j.Logger;
+
+import de.evjnw.jlk.data.DataModell;
+import de.evjnw.jlk.ui.View;
+
+/** 
+ * Dieser View zeigt die Eingabemaske für neue Datenobjekte an.
+ * @author Stephan Groß
+ *
+ */
+public class NewModelView implements View {
+
+	private static final Logger LOG = Logger.getLogger(NewModelView.class);
+	
+	private JPanel actionPane;
+	private JPanel detailPane;
+	
+	@Override
+	public void display(String verb, List<DataModell> models, List<Object> data) {
+		// TODO Auto-generated method stub
+		this.LOG.info("Verb:"+verb);
+	}
+
+	@Override
+	public void init(JPanel actionPane, JPanel detailPane) {
+		this.actionPane = actionPane;
+		this.detailPane = detailPane;
+	}
+
+}
