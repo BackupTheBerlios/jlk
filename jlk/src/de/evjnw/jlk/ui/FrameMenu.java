50493d53f367b7d30a70b1b5e3669e10c9937a6c
sgrossnw <sgrossnw>
2009-09-04 21:21:28 +0000

    27.7. Javadoc

--- jlk/src/de/evjnw/jlk/ui/FrameMenu.java
+++ jlk/src/de/evjnw/jlk/ui/FrameMenu.java
@@ -1,6 +1,6 @@
 /* 
    JLK - Java Lieder Katalog
-   Copyright 2008, Stephan Gross
+   Copyright 2008-2009, Stephan Gross
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-   $Id: FrameMenu.java,v 1.3 2009/08/15 11:55:33 sgrossnw Exp $
+   $Id: FrameMenu.java,v 1.4 2009/09/04 21:21:28 sgrossnw Exp $
  */
 package de.evjnw.jlk.ui;
 
