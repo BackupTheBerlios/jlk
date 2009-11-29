81d4308edfcc017dc64c517edbdf70eaaaf01160
ma08 <ma08>
2009-11-29 14:04:20 +0000

    *** empty log message ***

--- jlk/tests/de/evjnw/jlk/data/ZusammenstellungTest.java
+++ jlk/tests/de/evjnw/jlk/data/ZusammenstellungTest.java
@@ -1,3 +1,25 @@
+/* 
+   JLK - Java Lieder Katalog
+   Copyright 2009, Mario Aldag
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   $Id: ZusammenstellungTest.java,v 1.3 2009/11/29 14:04:20 ma08 Exp $
+ */
+/*
+ * Dieser Unittest testet die Properties von Zusammenstellung.java mit Zugriff auf die Liste Anhang
+ * 
+ */
 package de.evjnw.jlk.data;
 
 import junit.framework.TestCase;
